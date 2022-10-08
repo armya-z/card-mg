df4a65355bef38333b1be7a664fa06d70c21e117
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-08 15:11:50 +0330

    add: home page

---
+++ src/components/homepage/homepage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Card from "../../ui/card";
+
+function HomePage() {
+  return (
+    <>
+      <div className="App">
+        <Card
+          name="mellat"
+          number="1799 3684 2564 8642"
+          owner="ali akbar"
+        />
+
+        <Card
+          name="mellat"
+          number="1799 3684 2564 8642"
+          owner="ali akbar"
+        />
+
+        <Card
+          name="mellat"
+          number="1799 3684 2564 8642"
+          owner="ali akbar"
+        />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
