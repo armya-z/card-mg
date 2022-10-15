f0989b627f8d40f00689411b34cc796310b6a4b7
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-15 14:31:07 +0330

    update app.jsx: add trlist route

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,14 @@ import {
 } from "react-router-dom";
 import TrForm from "./components/transactions/trform";
 import HomePage from "./components/homepage/homepage";
+import Trlist from "./components/transactions/trlist";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route
-          exact
-          path="/transactions"
-          element={<transactions />}
-        />
+        <Route exact path="/trlist" element={<Trlist />} />
         <Route exact path="/trform" element={<TrForm />} />
       </Routes>
     </BrowserRouter>
