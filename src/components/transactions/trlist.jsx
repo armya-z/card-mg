1db87b81cfdb71c50f88b8e09bd52a2d658e3c98
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-15 14:31:37 +0330

    update trlist: add trform

---
+++ src/components/transactions/trlist.jsx
@@ -0,0 +1,142 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const FormComp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5px;
+  background-color: #0d1f56;
+  height: 8rem;
+  gap: 5px;
+`;
+
+const StyledInput = styled.input`
+  border-radius: 5px;
+  font-family: "Lalezar", cursive;
+  border-color: #000000;
+  border-width: 1px;
+  text-align: center;
+  height: 2rem;
+`;
+const Translist = styled.ul`
+  border-radius: 5px;
+  font-family: "Lalezar", cursive;
+  border-color: #000000;
+  text-align: center;
+`;
+const TransListItem = styled.li`
+  display: inline-flex;
+  margin: 10px;
+  flex-direction: column;
+  padding: 10px 0px;
+`;
+const TrRow = styled.div`
+  border-radius: 15px;
+  background-color: #0d1f56;
+  margin: 0px;
+  padding: 10px;
+  color: white;
+  align-items: center;
+`;
+const StyledButtom = styled.button`
+  border-bottom: 1px;
+  font-family: "Lalezar", cursive;
+  font-size: 0.8rem;
+  background-color: #ffffff;
+  color: black;
+  border-width: 1px;
+  text-align: center;
+  height: 2rem;
+  border-radius: 5px;
+`;
+const TrColoum = styled.h2`
+  margin: 5px;
+  flex-direction: column;
+  display: flex;
+  font-size: 1rem;
+  font-weight: 350;
+`;
+function Trlist() {
+  const [inputBankName, setInputBankName] = useState("");
+  const [inputCardNumber, setInputCardNumber] =
+    useState("");
+  const [inputCardOwner, setInputCardOwner] = useState("");
+  const [inputTransaction, setInputTransaction] =
+    useState("");
+  const [inputTransactionDate, setInputTransactionDate] =
+    useState("");
+  const [storage, setStorage] = useState([]);
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    setStorage((prev) => [
+      ...prev,
+      {
+        bankName: inputBankName,
+        cardNumber: inputCardNumber,
+        owner: inputCardOwner,
+        transaction: inputTransaction,
+        transactionDate: inputTransactionDate,
+      },
+    ]);
+
+    setInputBankName("");
+    setInputCardNumber("");
+    setInputCardOwner("");
+    setInputTransaction("");
+    setInputTransactionDate("");
+  };
+
+  return (
+    <>
+      <FormComp>
+        <StyledInput
+          type="text"
+          value={inputBankName}
+          onChange={(e) => setInputBankName(e.target.value)}
+          placeholder="نام بانک"
+        />
+        <StyledInput
+          type="text"
+          value={inputCardNumber}
+          onChange={(e) =>
+            setInputCardNumber(e.target.value)
+          }
+          placeholder="شماره کارت"
+        />
+        <StyledInput
+          type="text"
+          value={inputCardOwner}
+          onChange={(e) =>
+            setInputCardOwner(e.target.value)
+          }
+          placeholder="صاحب کارت"
+        />
+        <StyledInput
+          type="text"
+          value={inputTransaction}
+          onChange={(e) =>
+            setInputTransaction(e.target.value)
+          }
+          placeholder="مبلغ"
+        />
+        <StyledInput
+          type="text"
+          value={inputTransactionDate}
+          onChange={(e) =>
+            setInputTransactionDate(e.target.value)
+          }
+          placeholder="تاریخ"
+        />
+
+        <StyledButtom onClick={handlesubmit}>
+          "ثبت"
+        </StyledButtom>
+      </FormComp>
+    </>
+  );
+}
+
+export default Trlist;
