3b686e67ef441ed880353e7b44186def3ddc8aec
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-16 18:24:13 +0330

    add: remaining budget

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -9,18 +9,21 @@ function HomePage() {
           name="mellat"
           number="1799 3684 2564 8642"
           owner="ali akbar"
+          remaining="2,000,000,000  IRR"
         />
 
         <Card
           name="mellat"
           number="1799 3684 2564 8642"
           owner="ali akbar"
+          remaining="1,025,000,000  IRR"
         />
 
         <Card
           name="mellat"
           number="1799 3684 2564 8642"
           owner="ali akbar"
+          remaining="925,000,000  IRR"
         />
       </div>
     </>
