fa7e52b5b73a7aa268d471ad6980a9e1cf6657fd
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-20 01:54:26 +0330

    update: clean code

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,37 +1,8 @@
 import React from "react";
-import Card from "../../ui/card";
-import addcardui from "../transactions/addcardui";
 import AddCard from "../transactions/addcardui";
 
 function HomePage() {
-  return (
-    <>
-      <div className="App">
-        <AddCard />
-
-        <Card
-          name="mellat"
-          number="1799 3684 2564 8642"
-          owner="ali akbar"
-          remaining="2,000,000,000  IRR"
-        />
-
-        <Card
-          name="mellat"
-          number="1799 3684 2564 8642"
-          owner="ali akbar"
-          remaining="1,025,000,000  IRR"
-        />
-
-        <Card
-          name="mellat"
-          number="1799 3684 2564 8642"
-          owner="ali akbar"
-          remaining="925,000,000  IRR"
-        />
-      </div>
-    </>
-  );
+  return <AddCard />;
 }
 
 export default HomePage;
