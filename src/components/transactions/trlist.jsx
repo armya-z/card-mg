954cc9aac99cb9dbe8b8b9a1d979fea08eb341ae
armya-z <111483912+armya-z@users.noreply.github.com>
2022-10-22 15:27:29 +0330

    update: filter api

--- src/components/transactions/trlist.jsx
+++ src/components/transactions/trlist.jsx
@@ -19,6 +19,7 @@ const StyledInput = styled.input`
   border-width: 1px;
   text-align: center;
   height: 2rem;
+  width: 200px;
 `;
 const Translist = styled.ol`
   border-radius: 5px;
@@ -43,12 +44,13 @@ const TrRow = styled.div`
 const StyledButtom = styled.button`
   border-bottom: 1px;
   font-family: "Lalezar", cursive;
-  font-size: 0.8rem;
-  background-color: #ffffff;
-  color: black;
+  font-size: 1rem;
+  background-color: #22a657;
+  color: #ffffff;
   border-width: 1px;
   text-align: center;
-  height: 2rem;
+  width: 70px;
+  height: 40px;
   border-radius: 5px;
 `;
 const TrColoum = styled.h2`
@@ -58,8 +60,29 @@ const TrColoum = styled.h2`
   font-size: 1rem;
   font-weight: 350;
 `;
+const StyledSelect = styled.select`
+  width: 200px;
+  border-radius: 5px;
+  font-family: "Lalezar", cursive;
+  border-color: #000000;
+  border-width: 1px;
+  text-align: center;
+  height: 2rem;
+  option {
+    color: black;
+    background: white;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+`;
+
 //
 //
+const cardListUrl =
+  "https://634d1dd9acb391d34a944653.mockapi.io/api/v1/cardlist";
+
 const userList = axios.create({
   baseURL:
     "https://634d1dd9acb391d34a944653.mockapi.io/api/v1/",
@@ -70,11 +93,12 @@ function Trlist() {
     useState("");
   const [inputCardOwner, setInputCardOwner] = useState("");
   const [inputTransaction, setInputTransaction] =
-    useState("");
+    useState();
   const [inputTransactionDate, setInputTransactionDate] =
     useState("");
   const [cardData, setCardData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [cardList, setCardList] = useState([]);
 
   const handlesubmit = (event) => {
     event.preventDefault();
@@ -85,6 +109,7 @@ function Trlist() {
       transaction: inputTransaction,
       transactiondate: inputTransactionDate,
     };
+
     setIsLoading(true);
 
     axios
@@ -97,9 +122,10 @@ function Trlist() {
     setInputBankName("");
     setInputCardNumber("");
     setInputCardOwner("");
-    setInputTransaction("");
+    setInputTransaction();
     setInputTransactionDate("");
   };
+
   useEffect(() => {
     setIsLoading(false);
   }, [isLoading]);
@@ -109,34 +135,45 @@ function Trlist() {
       setCardData(Response.data);
     });
   }, [isLoading]);
+  useEffect(() => {
+    axios.get(cardListUrl).then((Response) => {
+      setCardList(Response.data);
+    });
+  }, []);
 
   return (
     <div>
       <FormComp>
+        <StyledSelect>
+          <option defaultValue="">نام بانک</option>
+          {cardList?.map((card) => (
+            <option key={card.id}>
+              {card.addBankNameInput}
+            </option>
+          ))}
+          ;
+        </StyledSelect>
+
+        <StyledSelect>
+          <option defaultValue="">شماره کارت</option>
+          {cardList?.map((card) => (
+            <option key={card.id}>
+              {card.addCardNumberInput}
+            </option>
+          ))}
+          ;
+        </StyledSelect>
+        <StyledSelect>
+          <option defaultValue="">صاحب کارت</option>
+          {cardList?.map((card) => (
+            <option key={card.id}>
+              {card.addCardOwnerInput}
+            </option>
+          ))}
+          ;
+        </StyledSelect>
         <StyledInput
-          type="text"
-          value={inputBankName}
-          onChange={(e) => setInputBankName(e.target.value)}
-          placeholder="نام بانک"
-        />
-        <StyledInput
-          type="text"
-          value={inputCardNumber}
-          onChange={(e) =>
-            setInputCardNumber(e.target.value)
-          }
-          placeholder="شماره کارت"
-        />
-        <StyledInput
-          type="text"
-          value={inputCardOwner}
-          onChange={(e) =>
-            setInputCardOwner(e.target.value)
-          }
-          placeholder="صاحب کارت"
-        />
-        <StyledInput
-          type="text"
+          type="number"
           value={inputTransaction}
           onChange={(e) =>
             setInputTransaction(e.target.value)
@@ -144,18 +181,18 @@ function Trlist() {
           placeholder="مبلغ"
         />
         <StyledInput
-          type="text"
+          type="date"
           value={inputTransactionDate}
           onChange={(e) =>
             setInputTransactionDate(e.target.value)
           }
           placeholder="تاریخ"
         />
-
         <StyledButtom onClick={handlesubmit}>
           "ثبت"
         </StyledButtom>
       </FormComp>
+
       <Translist>
         {cardData?.map((card) => (
           <TransListItem key={card.id}>
